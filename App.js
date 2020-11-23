700950bd5ee503c8d9d35318b7bc07c843d35e56
ahmetbasdan <aeb81tr@gmail.com>
2020-11-23 23:34:23 +0300

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { View, Text, TextInput, Button } from 'react-native'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+
+  state = {
+    inputDeger: '',
+    inputDegerArray: [],
+  }
+
+  inputDegisti(text) {
+    this.setState({ inputDeger: text })
+  }
+
+  kaydet() {
+    let yeniArray = this.state.inputDegerArray
+    yeniArray.push(this.state.inputDeger)
+
+    this.setState({ inputDegerArray: yeniArray })
+  }
+
+  render() {
+    return (
+      <View style={{ width: '100%', height: '100%', justifyContent: 'center', alignItems: 'center' }}>
+        <TextInput
+          style={{ width: 250, height: 50, borderWidth: 1 }}
+          value={this.state.inputDeger}
+          onChangeText={(text) => this.inputDegisti(text)}
+        />
+        <Button
+          title="Kaydet"
+          onPress={() => this.kaydet()}
+        />
+        {
+          this.state.inputDegerArray.map((value, index) => {
+            return (<Text key={index}>{value}</Text>)
+          })
+        }
+      </View>
+    )
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
